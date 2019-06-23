8f0d1bfafcc8598704dba7f2170803c105631213
carlosescom <carlos.escom.ipn@gmail.com>
2019-06-23 16:16:49 -0500

    IERC223 debía ser una interfaz

--- contracts/IERC223.sol
+++ contracts/IERC223.sol
@@ -5,12 +5,10 @@ pragma solidity 0.5.8;
  * @dev Carlos Noverón
  */
 
-contract IERC223 {
-  uint public totalSupply;
-
-  function balanceOf(address who) public returns (uint);
-  function transfer(address to, uint value) public;
-  function transfer(address to, uint value, bytes memory data) public;
+interface IERC223 {
+  function balanceOf(address who) external returns (uint);
+  function transfer(address to, uint value) external;
+  function transfer(address to, uint value, bytes calldata data) external;
 
   event Transfer(address indexed from, address indexed to, uint value, bytes data);
 }
