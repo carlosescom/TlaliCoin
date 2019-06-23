5a07bd44dfadcc3ef0e258bf6edf2e456de3bb1b
carlosescom <carlos.escom.ipn@gmail.com>
2019-06-23 01:12:46 -0500

    Base del token

---
+++ contracts/TlaliCoin.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.5.8;
+
+ /**
+ * @title TlaliCoin es un token que incentiva el reciclaje
+ * @dev Carlos Noverón https://github.com/carlosescom/TlaliCoin
+ */
+
+import "./IERC20.sol";
+import "./IERC223.sol";
+
+contract TlaliCoin is IERC20, IERC223 {
+
+}
