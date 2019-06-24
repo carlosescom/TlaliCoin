e2f03e0a1e0be3e8a53711ab5a8ecee3f06e6e52
carlosescom <carlos.escom.ipn@gmail.com>
2019-06-24 12:17:32 -0500

    + tokenFallback(address, uint, bytes)

--- contracts/ERC223_Receiver.sol
+++ contracts/ERC223_Receiver.sol
@@ -5,4 +5,12 @@ pragma solidity 0.5.8;
  */
 
 interface ERC223_Receiver {
+/**
+ * @dev Función estándar que manejará transacciones entrantes
+ *
+ * @param _from  Token sender address.
+ * @param _value Amount of tokens.
+ * @param _data  Transaction metadata.
+ */
+    function tokenFallback(address _from, uint _value, bytes _data);
 }
