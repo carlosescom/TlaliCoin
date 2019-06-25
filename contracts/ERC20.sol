38b4fa356f4f6437a9bb71122ff9d4b06bb417f8
carlosescom <carlos.escom.ipn@gmail.com>
2019-06-25 01:36:36 -0500

    - private _balances + balances

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -18,8 +18,7 @@ import "./SafeMath.sol";
 contract ERC20 is IERC20 {
   using SafeMath for uint256;
 
-  mapping(address => uint256) private _balances;
-
+  mapping(address => uint256) balances;
   mapping(address => mapping(address => uint256)) private _allowed;
 
   /**
